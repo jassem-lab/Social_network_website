f0f70322886fc03654f17f77f5edc3e87ef2d2af
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-02-04 14:05:45 +0100

    fix mongodb issue

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const userRoutes = require("./routes/user.routes");
 
 mongoose
   .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
