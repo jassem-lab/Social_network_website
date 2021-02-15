89717b9b43bb2c1e56f46b85dce79f5ae1ed6b2a
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-02-15 05:11:01 +0100

    auth middleware checkUser

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const UserModel = require("../models//UserModel");
+const UserModel = require("../models/user.model");
 
 module.exports.checkUser = (req, res, next) => {
   const token = req.cookie.jwt;
