49b3ba59a952a5e5d13ee2b167a61aec952ccff3
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-03-02 10:23:21 +0100

    save new Profile in DB

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -26,4 +26,20 @@ module.exports.uploadProfil = async (req, res) => {
       `${_dirname}/../client/public/uploads/profil/${fileName}`
     )
   );
+
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set: { picture: "./uploads/profil/" + fileName } },
+      { new: true, upset: true, setDefaultsOnInsert: true },
+      (err,docs) =>{
+        if(!err) 
+          if(!err) return res.send(docs) ; 
+          else return res.status(500).send({message : err})
+        
+      }
+    );
+  } catch (err) {
+    return res.status(500).send({message : err})
+  }
 };
